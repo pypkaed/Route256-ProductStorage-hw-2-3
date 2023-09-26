b377db5d0cf8e69d1e6867135a6fc61eba906057
pypkaed <pypka.eduardovna@yandex.ru>
2023-09-26 16:30:21 +0300

    [CSJHW-2]: make Product.cs implement IEquatable<Product>

--- Products/Products/Entities/Product.cs
+++ Products/Products/Entities/Product.cs
@@ -2,7 +2,7 @@ using Products.Models;
 
 namespace Products.Entities;
 
-public class Product
+public class Product : IEquatable<Product>
 {
     public Product(
         ProductId id,
@@ -29,4 +29,19 @@ public class Product
     public ProductCategory Category { get; }
     public DateTime ManufactureDate { get; init; }
     public WarehouseId WarehouseId { get; }
+
+    public bool Equals(Product? other)
+    {
+        return Id.Equals(other?.Id);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return Equals(obj as Product);
+    }
+
+    public override int GetHashCode()
+    {
+        return Id.GetHashCode();
+    }
 }
